47899cc95ab6179e88c0a69f279652034a7306dd
Davide <dt.turrini@gmail.com>
2021-02-02 16:28:58 +0100

    v0.31

--- setup_cms.sh
+++ setup_cms.sh
@@ -96,7 +96,7 @@ expect eof
 
 function addProject() {
 
-	NEW_ROOT_PASS=$(cat /home/ec2-user/mysql.root.password)
+	NEW_ROOT_PASS=$(</home/ec2-user/mysql.root.password)
 	until [[ ${PROJ_NAME} =~ ^[a-zA-Z0-9_-]+$ && ${PROJ_EXISTS} == '0' && ${#PROJ_NAME} -lt 16 ]]; do
 		read -rp "Project name: " -e PROJ_NAME
 		PROJ_EXISTS=$(grep -c -E "^### Project ${PROJ_NAME}\$" "/home/ec2-user/Caddyfile")
